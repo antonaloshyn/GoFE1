fd5715df53b36933e5091d9ab080b160aad2dc4b
antonaloshyn <antonaloshyn@gmail.com>
2016-05-02 13:47:29 +0300

    cv tmpl & carousel

---
+++ js11-12 my carousel_CV tmpl/template/js/script.js
@@ -0,0 +1,35 @@
+﻿'use strict'
+$(function(){
+
+	var html = $('#resume').html();
+	console.log(html);
+
+	var myResume = [
+			{	
+				name: 'Антон Алёшин',
+				img: './img/userpic.jpg',
+				info: 'Будущий повелитель тегов'
+			},
+			{	
+				reasons: ['Это круто',
+				'Люблю, когда на сайте всё красиво и двигается',
+				'Меня заставляют это делать мои кредиторы']
+			},
+			{
+			
+				tel: '050-050-50-50',
+				vk: 'https://vk.com/anton_aloshyn'
+			},
+			{
+			
+				feedback:'На меня можно положиться, если я сам сейчас на ком-то не лежу'
+			}
+		];
+
+	var content = tmpl(html,{
+		data: myResume
+	});
+
+	$('body').append(content);
+
+});
